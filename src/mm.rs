44159423a0003b425f56aec8a04383d0357f6c30
Youjie Zheng <zyj578335934@gmail.com>
2024-10-22 20:50:41 +0800

    [feat] Support complie and test musl app on rv64 and aarch64

--- src/mm.rs
+++ src/mm.rs
@@ -19,7 +19,10 @@ use crate::{config, loader};
 /// - The second return value is the top of the user stack.
 /// - The third return value is the address space of the user app.
 pub fn load_user_app(app_name: &str) -> AxResult<(VirtAddr, VirtAddr, AddrSpace)> {
-    let mut uspace = axmm::new_user_aspace()?;
+    let mut uspace = axmm::new_user_aspace(
+        VirtAddr::from_usize(config::USER_SPACE_BASE),
+        config::USER_SPACE_SIZE,
+    )?;
     let elf_info = loader::load_elf(app_name, uspace.base());
     for segement in elf_info.segments {
         debug!(
